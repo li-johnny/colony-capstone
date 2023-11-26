11f8c3f8b98602fa35e9be5dd899d01e01537411
Andrew Vester <andrewsvester@gmail.com>
2023-11-25 21:18:37 -0800

    Server Accepting Images as Input

--- pythonServer.py
+++ pythonServer.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD'] = 'static\Image'
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def home():
     return render_template("index.html")
+
+@app.route("/", methods=['POST'])
+def getImage():
+    
+    file = request.files.get('img')
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD'], filename))
+    img = os.path.join(app.config['UPLOAD'], filename)
+    return render_template("index.html",image = img)
 
 if __name__ == '__main__':
     app.run(debug=True)
